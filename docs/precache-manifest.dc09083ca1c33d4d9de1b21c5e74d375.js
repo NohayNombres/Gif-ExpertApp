4ae60f7d6613f2ad9fd79a23ed3af57f715775f6
Cristian Quiroga <cristiancquiroga380@gmail.com>
2021-03-08 21:16:49 -0500

    Add docs

---
+++ docs/precache-manifest.dc09083ca1c33d4d9de1b21c5e74d375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895564838f0d6908a37c947557c7b5a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31bd9040256d4673b584",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31bd9040256d4673b584",
+    "url": "/static/js/main.7e898a72.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
